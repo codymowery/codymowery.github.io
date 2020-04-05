b1694734496224a41525267c1f2a1d6989730948
Cody Mowery <mowery.cody@gmail.com>
2020-04-05 11:13:45 -0700

    update map

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(37.762975, -122.457853), // San Francisco
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
